e8f85bc4f2d3af4f5196170caf1d9cd428155061
SebastianWille <Cibbinator@gmx.de>
2021-02-06 13:13:05 +0100

    1_SW_CreateMXTSummary2.01.py updated according to new trejectory output format, 2_SW_AnalyzePESTrajectory2.05.py added to count for new trajectory output when generating data for plotting

--- scripts/1_SW_CreateMXTSummary2.01.py
+++ scripts/1_SW_CreateMXTSummary2.01.py
@@ -30,29 +30,30 @@ VERSION_ID = 2.01
 class Traj:
 	def __init__(self, fname, ekin_p_i, ekin_l_i, epot_i, etotal_i,\
 			r_p_i, v_p_i, polar_i, azi_i, ekin_p_f, ekin_l_f, epot_f,   \
-                        etotal_f, r_p_f, polar_f, azi_f, time, turn_pnts, \
-			cl_appr, r_p_min, traj_id):
-		self.fname    = fname
-		self.ekin_p_i = ekin_p_i
-		self.ekin_l_i = ekin_l_i
-		self.epot_i   = epot_i
-		self.etotal_i = etotal_i
-		self.r_p_i    = r_p_i
-		self.v_p_i    = v_p_i
-		self.polar_i  = polar_i
-		self.azi_i    = azi_i
-		self.ekin_p_f = ekin_p_f
-		self.ekin_l_f = ekin_l_f
-		self.epot_f   = epot_f
-		self.etotal_f = etotal_f
-		self.r_p_f    = r_p_f
-		self.polar_f  = polar_f
-		self.azi_f    = azi_f
-		self.time     = time
-		self.turn_pnts= turn_pnts
-		self.cl_appr  = cl_appr
-		self.r_p_min  = r_p_min
-                self.traj_id  = traj_id
+                        etotal_f, r_p_f, v_p_f, polar_f, azi_f, time, turn_pnts, \
+			cl_appr, cl_appr_t, r_p_min, traj_id):
+		self.fname     = fname
+		self.ekin_p_i  = ekin_p_i
+		self.ekin_l_i  = ekin_l_i
+		self.epot_i    = epot_i
+		self.etotal_i  = etotal_i
+		self.r_p_i     = r_p_i
+		self.v_p_i     = v_p_i
+		self.polar_i   = polar_i
+		self.azi_i     = azi_i
+		self.ekin_p_f  = ekin_p_f
+		self.ekin_l_f  = ekin_l_f
+		self.epot_f    = epot_f
+		self.etotal_f  = etotal_f
+		self.r_p_f     = r_p_f
+		self.polar_f   = polar_f
+		self.azi_f     = azi_f
+		self.time      = time
+		self.turn_pnts = turn_pnts
+		self.cl_appr   = cl_appr
+		self.cl_appr_t = cl_appr_t
+		self.r_p_min   = r_p_min
+                self.traj_id   = traj_id
 
 	def containsNaN(self):
 		return "NaN" in self.ekin_p_i or "NaN" in self.ekin_l_i or "NaN" in self.epot_i or "NaN" in self.etotal_i or \
@@ -79,6 +80,7 @@ class Traj:
                         "\ntime " + str(self.time) +\
                         "\nturn_pnts " + str(self.turn_pnts) +\
 			"\ncl_appr " + str(self.cl_appr) +\
+			"\ncl_appr_t " + str(self.cl_appr_t) +\
 			"\nr_p_min " + str(self.r_p_min) +\
                         "\ntraj_id " + str(self.traj_id)
 
@@ -136,6 +138,7 @@ def read_in_mxt_fins(logfile):
 				        epot_i   = line.strip(' \n\t\r').split()[-1]; line=infile.next()	#	epot_i   =   -392.2094094
 				        etotal_i = line.strip(' \n\t\r').split()[-1]; line=infile.next()	#       etotal_i =   -369.3305490
 			        	r_p_i    = line.strip(' \n\t\r').split()[-3:] ; line=infile.next()	#	r_i      =     30.1309358    32.4013593     3.5000000
+			        	v_p_i    = line.strip(' \n\t\r').split()[-3:] ; line=infile.next()	#	v_i      =     0.0000000     0.0000000     -0.1934888
 		        		polar_i  = line.strip(' \n\t\r').split()[-1]; line=infile.next()	#	polar_i  =     50.0000000
 	        			azi_i    = line.strip(' \n\t\r').split()[-1]; line=infile.next()	#	azi_i    =      0.0000000
 		        		line=infile.next()							#
@@ -144,18 +147,20 @@ def read_in_mxt_fins(logfile):
        	                	        epot_f   = line.strip(' \n\t\r').split()[-1]; line=infile.next()	#	epot_f   =   -391.8832698
        	        	                etotal_f = line.strip(' \n\t\r').split()[-1]; line=infile.next()	#	etotal_f =   -369.3305177
        	        	                r_p_f    = line.strip(' \n\t\r').split()[-3:]; line=infile.next()	#	r_f      =     33.1131979    32.3776858     1.3585263
+       	        	                v_p_f    = line.strip(' \n\t\r').split()[-3:]; line=infile.next()	#	r_f      =      0.0026410     0.0117354     0.1513796
        	 		                polar_f  = line.strip(' \n\t\r').split()[-1]; line=infile.next()	#	polar_f  =    122.6747121
        		       	                azi_f    = line.strip(' \n\t\r').split()[-1]; line=infile.next()	#	azi_f    =     -5.5773256
 				        line=infile.next()
 				        time     = line.strip(' \n\t\r').split()[-1]; line=infile.next()	#	time     =     20.0000000
 				        turn_pnts= line.strip(' \n\t\r').split()[-1]; line=infile.next()	#	turn_pnts =           1
 				        cl_appr  = line.strip(' \n\t\r').split()[-1]; line=infile.next()        #       cl_appr  =      0.9694690
+				        cl_appr_t  = line.strip(' \n\t\r').split()[-1]; line=infile.next()        #       cl_appr_time  =          278
 				        r_p_min  = line.strip(' \n\t\r').split()[-3:]				#	r_min_p  =     51.3246421    33.1600141     0.9996723
 
 				        traj = Traj(folder, ekin_p_i, ekin_l_i, epot_i, etotal_i,  \
-                      			    r_p_i, polar_i, azi_i, ekin_p_f, ekin_l_f, epot_f,\
-                       			    etotal_f, r_p_f, polar_f, azi_f, time, turn_pnts,\
-					    cl_appr, r_p_min, traj_id)
+                      			    r_p_i, v_p_i, polar_i, azi_i, ekin_p_f, ekin_l_f, epot_f,\
+                       			    etotal_f, r_p_f, v_p_f, polar_f, azi_f, time, turn_pnts,\
+					    cl_appr, cl_appr_t, r_p_min, traj_id)
 			        except:
 				        print "Error in file ", folder, " in line ", line
                                         logfile.write("Error in file {} in line {}\n".format(folder,line))
@@ -181,13 +186,15 @@ def read_in_mxt_fins(logfile):
 
 def write_summary(logfile, outfile_name, traj_list):
 	outfile = open(outfile_name, "w")
-	outfile.write("# traj_id E_kin_p   E_kin_l        E_pot      E_total r_p(    x,        y,         z)     polar       azi   E_kin_p  E_kin_l       E_pot       E_total     r_p(x,       y,         z)       polar       azi     simtime turn_pnts   cl_appr   r_p_min\n")
+	outfile.write("# traj_id E_kin_p   E_kin_l        E_pot      E_total r_p(    x,        y,         z) v_p(    x,        y,         z)      polar       azi   E_kin_p  E_kin_l       E_pot       E_total     r_p(x,       y,         z) v_p(x,       y,         z)        polar       azi     simtime turn_pnts   cl_appr   cl_appr_t   r_p_min\n")
 	for traj in traj_list:
 		try:
-			outfile.write("%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s\n" \
+			outfile.write("%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s\n" \
 				% ( traj.traj_id, traj.ekin_p_i, traj.ekin_l_i, traj.epot_i, traj.etotal_i, traj.r_p_i[0], traj.r_p_i[1], traj.r_p_i[2], \
+                                traj.v_p_i[0], traj.v_p_i[1], traj.v_p_i[2], \
 				traj.polar_i, traj.azi_i, traj.ekin_p_f, traj.ekin_l_f, traj.epot_f, traj.etotal_f, traj.r_p_f[0], \
-				traj.r_p_f[1], traj.r_p_f[2], traj.polar_f, traj.azi_f, traj.time, traj.turn_pnts, traj.cl_appr, \
+				traj.r_p_f[1], traj.r_p_f[2], traj.v_p_f[0], traj.v_p_f[1], traj.v_p_f[2], traj.polar_f, traj.azi_f, \
+                                traj.time, traj.turn_pnts, traj.cl_appr, traj.cl_appr_t, \
 				traj.r_p_min[0], traj.r_p_min[1], traj.r_p_min[2]))
 		except (IndexError):
 			outfile.close()
@@ -225,6 +232,7 @@ def remove_unfinished_traj(logfile):
     os.chdir("../")
 
 
+# 2DO: include write function to spare unnecessary code!!
 def write_statistics(traj_list):
         outfile_15 = open("MXT2Summary_15.txt", "w")
         outfile_30 = open("MXT2Summary_30.txt", "w")
@@ -275,6 +283,7 @@ logfile = open(logfilename, 'w')
 print("Screen output will be automatically written to {}!".format(logfilename))
 
 
+# WARNING: the following is outdated since now empty or unfinished trajectory files are just skipped which is wanted!!
 # check if unfinished trajectories are there and remove them
 #remove_unfinished_traj(logfile)
 
